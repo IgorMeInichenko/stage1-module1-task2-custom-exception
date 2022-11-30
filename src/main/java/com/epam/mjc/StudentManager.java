31e8c66b7e22fed7d7b6f06378f7cc060049b01d
Igor Melnichenko <imelnicenko498@gmail.com>
2022-11-30 13:33:00 +0300

    Ready

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -5,10 +5,18 @@ public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+  public Student find (long studentID) throws myException {
+    Student result = Student.getValueOf(studentID);
+    if (result==null){
+      throw new myException("Could not find student with ID "+studentID);
+    }
+    return result;
   }
-
+  public class myException extends IllegalArgumentException{
+    public myException (String message){
+      super(message);
+    }
+  }
   public static void main(String[] args) {
     StudentManager manager = new StudentManager();
 
